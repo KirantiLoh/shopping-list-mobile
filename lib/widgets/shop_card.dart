222d99cb654d1da83a64833dd70ed8cba56bbb8c
Maurice Yang <mauriceyangmy28@gmail.com>
2023-11-08 13:47:36 +0700

    Kelar tutorial 7

--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list_mobile/shoplist_form.dart';
+import 'package:shopping_list_mobile/screens/shoplist_form.dart';
 import 'package:shopping_list_mobile/types/type.dart';
 
 class ShopCard extends StatelessWidget {
