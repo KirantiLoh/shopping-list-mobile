934530344a09e9f9cad4afedc861283b529a9be2
Maurice Yang <mauriceyangmy28@gmail.com>
2023-11-08 13:44:33 +0700

    Kelar tutorial 7

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-
-class ShopItem {
-  final String name;
-  final IconData icon;
-
-  ShopItem(this.name, this.icon);
-}
+import 'package:shopping_list_mobile/types/type.dart';
+import 'package:shopping_list_mobile/widgets/left_drawer.dart';
+import 'package:shopping_list_mobile/widgets/shop_card.dart';
 
 final List<ShopItem> items = [
   ShopItem("Lihat Produk", Icons.checklist),
@@ -24,6 +20,7 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Colors.indigo,
         foregroundColor: Colors.white,
       ),
+      drawer: const LeftDrawer(),
       body: SingleChildScrollView(
         child: Padding(
             padding: const EdgeInsets.all(8),
@@ -57,51 +54,6 @@ class MyHomePage extends StatelessWidget {
                 ),
               ],
             )),
-      ),
-    );
-  }
-}
-
-class ShopCard extends StatelessWidget {
-  final ShopItem item;
-
-  const ShopCard(this.item, {super.key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.indigo,
-      child: InkWell(
-        // Area responsive terhadap sentuhan
-        onTap: () {
-          // Memunculkan SnackBar ketika diklik
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
-        child: Container(
-          // Container untuk menyimpan Icon dan Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
       ),
     );
   }
